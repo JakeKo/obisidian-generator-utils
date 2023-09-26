3ecaa41705663e1ab631533fbbf8dacc6aabf770
Jake Koperski <wijakoperski@gmail.com>
2023-09-25 21:19:14 -0500

    Remove annotation model

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { AnnotationModal, TopicModal } from "src/modals";
+import { TopicModal } from "src/modals";
 import { Plugin } from "obsidian";
 
 export default class GeneratorUtils extends Plugin {
 	async onload() {
-		this.addCommand({
-			id: "annotation",
-			name: "Annotation",
-			callback: () => {
-				new AnnotationModal(this.app).open();
-			},
-		});
+		// this.addCommand({
+		// 	id: "annotation",
+		// 	name: "Annotation",
+		// 	callback: () => {
+		// 		new AnnotationModal(this.app).open();
+		// 	},
+		// });
 
 		this.addCommand({
 			id: "topic",
